d85eb9e4add4091946f99645acfa006dbd7f2e5f
Vixel V Vixel <33169104+Tqcoz@users.noreply.github.com>
2019-04-29 18:40:12 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
 const Discord = require('discord.js')
-const client = new Discord.Client({autoRecconect:true})
+const client = new Discord.Client({autoReconnect:true})
 
 
 
 
 client.on('message', message => {
 const prefix = "/";
+    
+    if (message.channel.id == ['572551812931125248', '572072693915058208']) {
+     const sayMessage = args.join(" ");
+
+      let servIcon = message.guild.iconURL;
+      let esayEmbed = new Discord.RichEmbed()
+      .setTitle("New Announcement")
+      .setColor("#0537ff")
+      .setThumbnail(servIcon)
+      .setDescription(`Written by ${message.author}`)
+      .addField("Message", `${sayMessage}`)
+      .setTimestamp();
+
+      const esayMessage = args.join(" ");
+      message.delete().catch(O_o=>{});
+
+      message.channel.send(esayEmbed);
+  }   
+    }
+    
     if (message.author.bot) return;
     const args = message.content.slice(prefix.length).trim().split(/ +/g)
     const command = args.shift().toLowerCase();
+    
     
     if (message.content.indexOf(prefix) !== 0) return;
 
